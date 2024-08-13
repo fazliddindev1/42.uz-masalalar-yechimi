2707a0daff20cc4969e12f96d5517eb6f8f53f7d
Fazliddin <149813353+fazliddindev1@users.noreply.github.com>
2024-08-13 22:29:24 +0500

    Add files via upload

---
+++ 14) linked Listni teskari qilish.py
@@ -0,0 +1,17 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def reverse(head):
+    prev = None
+    curr = head
+
+    while curr:
+        next_node = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next_node
+
+    return prev
