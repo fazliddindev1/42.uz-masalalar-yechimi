7d3d22724746bf99eea3abfe0ef2eaeea1272196
Fazliddin <149813353+fazliddindev1@users.noreply.github.com>
2024-09-16 11:43:19 +0500

    Update 001 nollarni ortga surish.py

--- 001 nollarni ortga surish.py
+++ 001 nollarni ortga surish.py
@@ -1,1 +1,11 @@
+def moveZeroes(nums: list) -> list:
+    count = 0
 
+    for i, num in enumerate(nums):
+        if num == 0:
+            count = count + 1
+            continue
+
+        nums[i], nums[i - count] = nums[i - count], nums[i]
+
+    return nums
