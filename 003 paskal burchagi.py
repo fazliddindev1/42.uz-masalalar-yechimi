9fe0329e381979616882f77962887c2cb2f5176f
Fazliddin <149813353+fazliddindev1@users.noreply.github.com>
2024-09-20 14:15:02 +0500

    Update 003 paskal burchagi.py

--- 003 paskal burchagi.py
+++ 003 paskal burchagi.py
@@ -1,1 +1,20 @@
+def generate_row(prev):
+    next_ = [1]
+    for i in range(len(prev) - 1):
+        next_.append(prev[i] + prev[i + 1])
+    next_.append(1)
+    return next_
 
+
+def generate(n: int) -> list:
+    if n == 0:
+        return []
+
+    row = [1]
+    result = [row]
+
+    for i in range(n - 1):
+        row = generate_row(row)
+        result.append(row)
+
+    return result
